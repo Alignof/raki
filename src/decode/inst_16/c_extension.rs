97ff59997f07451486a8f7e6a799eb6d276f869d
Alingof <Alignof@outlook.com>
2023-10-12 18:48:19 +0900

    [fix] remove `C_EBREAK` from parse_rd

--- src/decode/inst_16/c_extension.rs
+++ src/decode/inst_16/c_extension.rs
@@ -134,7 +134,6 @@ pub fn parse_rd(inst: u16, opkind: &OpcodeKind) -> Result<Option<usize>, Decodin
         | OpcodeKind::C_LWSP
         | OpcodeKind::C_LDSP
         | OpcodeKind::C_MV
-        | OpcodeKind::C_EBREAK
         | OpcodeKind::C_JALR
         | OpcodeKind::C_ADD => Ok(Some(q2_rd)),
         _ => Ok(None),

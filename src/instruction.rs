2d0d38915e87347029e6a7740d4b74836bd05d43
Alingof <Alignof@outlook.com>
2023-10-12 15:14:27 +0900

    [add] implement Display::fmt for Instruction

--- src/instruction.rs
+++ src/instruction.rs
@@ -2,6 +2,8 @@
 
 mod opcode;
 
+use std::fmt::{self, Display, Formatter};
+
 /// Instruction
 #[derive(Debug)]
 pub struct Instruction {
@@ -19,6 +21,78 @@ pub struct Instruction {
     pub extension: Extensions,
     /// Instruction format
     pub inst_format: InstFormat,
+}
+
+impl Display for Instruction {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self.inst_format {
+            _ => {
+                write!(
+                    f,
+                    "{}{}{}{}{}",
+                    self.opc.to_string(),
+                    if let Some(rd) = self.rd {
+                        format!(" {}", reg2str(rd))
+                    } else {
+                        format!("")
+                    },
+                    if let Some(rs1) = self.rs1 {
+                        format!(" {}", rs1)
+                    } else {
+                        format!("")
+                    },
+                    if let Some(rs2) = self.rs2 {
+                        format!(" {}", rs2)
+                    } else {
+                        format!("")
+                    },
+                    if let Some(imm) = self.imm {
+                        format!(" {}", imm)
+                    } else {
+                        format!("")
+                    },
+                )
+            }
+        }
+    }
+}
+
+fn reg2str(rd_value: usize) -> &'static str {
+    match rd_value {
+        0 => "zero",
+        1 => "ra",
+        2 => "sp",
+        3 => "gp",
+        4 => "tp",
+        5 => "t0",
+        6 => "t1",
+        7 => "t2",
+        8 => "s0", // fp
+        9 => "s1",
+        10 => "a0",
+        11 => "a1",
+        12 => "a2",
+        13 => "a3",
+        14 => "a4",
+        15 => "a5",
+        16 => "a6",
+        17 => "a7",
+        18 => "s2",
+        19 => "s3",
+        20 => "s4",
+        21 => "s5",
+        22 => "s6",
+        23 => "s7",
+        24 => "s8",
+        25 => "s9",
+        26 => "s10",
+        27 => "s11",
+        28 => "t3",
+        29 => "t4",
+        30 => "t5",
+        31 => "t6",
+        _ => panic!("unknown register"),
+    }
 }
 
 /// RISC-V extensions
@@ -230,43 +304,5 @@ impl Instruction {
         } else {
             "--"
         }
-    }
-}
-
-fn reg2str(rd_value: usize) -> &'static str {
-    match rd_value {
-        0 => "zero",
-        1 => "ra",
-        2 => "sp",
-        3 => "gp",
-        4 => "tp",
-        5 => "t0",
-        6 => "t1",
-        7 => "t2",
-        8 => "s0", // fp
-        9 => "s1",
-        10 => "a0",
-        11 => "a1",
-        12 => "a2",
-        13 => "a3",
-        14 => "a4",
-        15 => "a5",
-        16 => "a6",
-        17 => "a7",
-        18 => "s2",
-        19 => "s3",
-        20 => "s4",
-        21 => "s5",
-        22 => "s6",
-        23 => "s7",
-        24 => "s8",
-        25 => "s9",
-        26 => "s10",
-        27 => "s11",
-        28 => "t3",
-        29 => "t4",
-        30 => "t5",
-        31 => "t6",
-        _ => panic!("unknown register"),
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -25,3 +25,21 @@ pub enum Isa {
     Rv32,
     Rv64,
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn display_test() {
+        use crate::decode::Decode;
+        use crate::instruction::Instruction;
+        use crate::Isa;
+
+        let inst: u32 = 0b1110_1110_1100_0010_1000_0010_1001_0011;
+        let inst: Instruction = match inst.decode(Isa::Rv32) {
+            Ok(inst) => inst,
+            Err(e) => panic!("decoding failed due to {e:?}"),
+        };
+
+        println!("{inst}");
+    }
+}

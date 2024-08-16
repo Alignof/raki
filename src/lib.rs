b8a29458d20ba2ba1959806f51eab3398a825ede
Alingof <Alignof@outlook.com>
2024-08-16 14:39:32 +0900

    [fix] remove display test from lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -71,60 +71,6 @@ enum Extensions {
 #[cfg(test)]
 mod tests {
     #[test]
-    fn display_32bit_test() {
-        use crate::decode::Decode;
-        use crate::instruction::Instruction;
-        use crate::Isa;
-
-        let instructions: [u32; 8] = [
-            0b1110_1110_1100_0010_1000_0010_1001_0011,
-            0b110_1001_1000_0001_1000_0111_1001_0011,
-            0b1_1100_1110_0100_0010_0110_1010_1111,
-            0b1111_0101_0000_0000_0000_0000_1111,
-            0b1000_1111_0100_0010_0111_1010_1111,
-            0b111_0011,
-            0b110_0101_1111_0000_0011_0000_1110_1111,
-            0b110_1001_0111,
-        ];
-
-        for inst in &instructions {
-            let inst: Instruction = match inst.decode(Isa::Rv32) {
-                Ok(inst) => inst,
-                Err(e) => panic!("decoding failed due to {e:?}"),
-            };
-
-            println!("{inst}");
-        }
-    }
-
-    #[test]
-    fn display_16bit_test() {
-        use crate::decode::Decode;
-        use crate::instruction::Instruction;
-        use crate::Isa;
-
-        let instructions: [u16; 8] = [
-            0b1110_0100_0010_0110,
-            0b110_1100_0000_0100,
-            0b1110_0101_0001_0001,
-            0b1000_0101_0010_0110,
-            0b1011_0111_1111_0101,
-            0b11_0000,
-            0b1000_0000_1000_0010,
-            0b11_0111_1111_1101,
-        ];
-
-        for inst in &instructions {
-            let inst: Instruction = match inst.decode(Isa::Rv64) {
-                Ok(inst) => inst,
-                Err(e) => panic!("decoding failed due to {e:?}"),
-            };
-
-            println!("{inst}");
-        }
-    }
-
-    #[test]
     fn inst_eq_test() {
         use crate::decode::Decode;
         use crate::instruction::{base_i::BaseIOpcode, InstFormat, Instruction, OpcodeKind};
